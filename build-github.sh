d02adfd6396fb351d8e840558548e0370daad855
Daniel Hofer <daniel@daho.at>
2022-05-17 18:45:34 +0200

    RTFM

--- build-github.sh
+++ build-github.sh
@@ -1,1 +1,4 @@
-ng build --base-href ./LNDF2022-Password-Interception-Demo --deploy-url ./LNDF2022-Password-Interception-Demo
+ng build --base-href ./LNDF2022-Password-Interception-Demo
+rm -r docs/*
+cp -r dist/LNDFPasswordDemo/* docs/
+git add docs/*
